bcbd4b91d67f1d5550723eb0c4aa1f7c6c1f17ea
Edilson Rodrigues <edarodrigs@gmail.com>
2020-10-16 07:26:23 -0300

    create database and config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json());
 
+/* app.post('/users/:id', (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
+    return response.json({ message: 'hello world' });
+}); */
+
+app.get('/users', (request, response) => {
+    return response.json({ message: 'hello world' });
 });
 
 app.listen(3333);
