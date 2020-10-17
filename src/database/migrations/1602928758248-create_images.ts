35a769afcd0ca46a40c9d58b26035ddd7df4ff6c
Edilson Rodrigues <edarodrigs@gmail.com>
2020-10-17 06:59:36 -0300

    create images

---
+++ src/database/migrations/1602928758248-create_images.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImages1602928758248 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
