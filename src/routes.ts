b00e95c720bf1defa50c37be877cc5e18a1acce5
Edilson Rodrigues <edarodrigs@gmail.com>
2020-10-17 06:58:01 -0300

    detalhe do orfanato

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ const routes = Router();
 //Controllers - onde irá ficar nossa logica
 
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
